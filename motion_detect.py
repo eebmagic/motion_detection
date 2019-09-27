937f02ca373740e628b57ae8a0d0096744db97f0
Ethan B <eebmagic@gmail.com>
2019-09-26 22:15:02 -0400

    removed unused vars and funcs

--- motion_detect.py
+++ motion_detect.py
@@ -15,8 +15,12 @@ import numpy as np
 
 ########################################################################
 # SETTINGS #
-SHOW_REAL_VIDEO = ("-s" in sys.argv[:] or "--show" in sys.argv[:])   # Set this to True to get real camera video from cv2
-SHOW_START_FRAME = ("-i" in sys.argv[:] or "--initial" in sys.argv[:])  # Check for option for first frame
+# Set this to True to get real camera video from cv2
+SHOW_REAL_VIDEO = ("-s" in sys.argv[:] or "--show" in sys.argv[:])
+
+# Check for option for first frame
+SHOW_START_FRAME = ("-i" in sys.argv[:] or "--initial" in sys.argv[:])
+
 
 # Max pixel difference over which to trigger the motion alarm
 THRESHOLD = 100
@@ -38,10 +42,6 @@ def format_frame(inputFrame):
     # return gray[0:-150, 450:-560]
 
 
-def count_different_pixels(frame_a, frame_b):
-    pass
-
-
 # Start of main
 
 cap = cv2.VideoCapture(0)
@@ -54,20 +54,9 @@ print(start_frame)
 if SHOW_START_FRAME:
     cv2.imshow("Start Frame", start_frame)
 
-# next_frame = format_frame(cap.read()[1])
-# print(next_frame)
-
-# diff = cv2.absdiff(start_frame, next_frame)
-# print("\nAbsDifference Frame:")
-# print(diff)
-# quit()
-
 
 SUM_FRAME = start_frame.astype(np.uint32)
 AVG_FRAME = start_frame.astype(np.uint32)
-
-SUM_CHANGE = 0
-AVG_CHANGE = 0
 
 counter = 1
 
@@ -75,35 +64,20 @@ while(cv2.waitKey(1) & 0xFF != ord('q')):
     # Get image data
     frame = format_frame(cap.read()[1])
 
-    # DIFF = np.absolute(AVG_FRAME - frame)
     DIFF = cv2.absdiff(np.float32(AVG_FRAME), np.float32(frame))
-    CHANGE = int(DIFF.sum())
     MAX_DIFF = np.amax(DIFF)
 
     print(counter)
-    # print(int(SUM_FRAME.sum()))
-    # print(SUM_FRAME)
-    # print(int(AVG_FRAME.sum()))
-    # print(AVG_FRAME)
-    # print(f"frame.sum:  {frame.sum()}")
-    print(f"CHANGE:     {CHANGE}")
-    print(f"SUM_CHANGE: {SUM_CHANGE}")
-    print(f"AVG_CHANGE: {AVG_CHANGE}\n")
     print(f"MAX_DIFF: {MAX_DIFF}\n")
 
     # Check for change dist from avg
     if MAX_DIFF > THRESHOLD:
         print("\t\t\tMOVEMENT DETECTED!")
 
-    # Check for flukes
     # Update AVG and SUM frames
-    # if frame.sum() < 30_000_000:
     counter += 1
     SUM_FRAME = np.add(SUM_FRAME, frame)
     AVG_FRAME = (SUM_FRAME / counter).astype(np.uint8)
-
-    SUM_CHANGE += CHANGE
-    AVG_CHANGE = int(SUM_CHANGE / counter)
     
     # Display the resulting frame
     if SHOW_REAL_VIDEO:
